5d3d413189bb4deedca5b22d902f9ede67931d84
sprtd <sagesdn@gmail.com>
2021-09-19 01:19:18 +0100

    Test owner-only capacity to add item

--- test/SupplyChain.test.js
+++ test/SupplyChain.test.js
@@ -17,9 +17,6 @@ contract('SupplyChain', async  accountsPayload => {
     it('Sets sku to zero on deployment', async () => {
       const skuCount = await supplyChain.getSkuCount()
       const deployerAccount = await supplyChain.getOwner()
-
-      console.log('counter', skuCount)
-
       assert.equal(skuCount, 0)
       console.log('deployer address', deployerAccount)
       assert.equal(deployerAccount, owner)
@@ -27,13 +24,40 @@ contract('SupplyChain', async  accountsPayload => {
 
   })
 
-  contract('Transaction', () => {
-    const REVERT  = 'VM Exception while processing transaction: revert Only owner can call this function'
+  contract('Revert Non-Owner', () => {
     it('Disallows non-owner account from adding new item', async() => {
-      await truffleAssert.reverts(
-        supplyChain.addItem('Alpha1', 10, { from: addr1 }), REVERT
-      )
+      const REVERT  = 'Returned error: VM Exception while processing transaction: revert Only owner can call this function'
+      try {
+        await supplyChain.addItem('Alpha1', 10, { from: addr1})
+        throw null;
+        
+      } catch(err) {
+        assert(err.message.startsWith(REVERT), `Expected ${REVERT} but got ${err.message} instead`)
+        
+      }
 
     })
+  })
+
+  contract('Add Item', () => {
+    it('Allows only owner account to add item', async() => {
+      const nameBefore = 'Alpha 1'
+      const priceBefore = 10
+      let buyer = 0x0000000000000000000000000000000000000000
+      const skuCountBefore = await supplyChain.getSkuCount()
+      await supplyChain.addItem(nameBefore, priceBefore, { from: owner })
+      const skuCountAfter = await supplyChain.getSkuCount()
+      
+      const newlyAddedItems  = await supplyChain.fetchItems(skuCountAfter)
+      const { name: nameAfter, price: priceAfter, status: statusAfter, seller: sellerAfter, buyer: buyerAfter, sku: skuAfter } = newlyAddedItems
+      console.log('name after addition', nameAfter)
+      assert(nameAfter, nameBefore)
+      assert(priceAfter, priceBefore)
+      assert(skuCountBefore, skuCountBefore.toNumber() + 1)
+      assert(statusAfter, 'For Sale')
+      assert(sellerAfter, owner)
+      assert(buyerAfter, buyer)
+    })
+
   })
 })
